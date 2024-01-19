ee9955b21b224e68bca7d9e9aac20c53cb943655
rcsen <rohansen856@gmail.com>
2024-01-19 07:59:22 +0530

    modified:   lib/main.dart 	modified:   lib/screens/main_screen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'package:admin/controllers/MenuAppController.dart';
-import 'package:admin/database/routine_database.dart';
 // import 'package:admin/screens/auth/auth_screen.dart';
 // import 'package:admin/screens/auth/splash_screen.dart';
 import 'package:admin/screens/main_screen.dart';
@@ -7,13 +5,12 @@ import 'package:admin/theme/theme_data.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox('testBox');
-  WidgetsFlutterBinding.ensureInitialized();
 
   const String supabaseUrl = "https://yyivbsxjptvxhmxtrqug.supabase.co";
   const String supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5aXZic3hqcHR2eGhteHRycXVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ5NDgzMjAsImV4cCI6MjAyMDUyNDMyMH0.vnxZenLPuh9kXe2xcTYb4FMVCmTgKy7EcvE-CBexvP4";
@@ -22,8 +19,6 @@ Future<void> main() async{
     url: supabaseUrl,
     anonKey: supabaseAnonKey,
   );
-
-  await RoutineDatabase.initDatabase();
 
   runApp(MyApp());
 }
@@ -47,14 +42,7 @@ class MyApp extends StatelessWidget {
       routes: <String, WidgetBuilder>{
         // '/': (_) => SplashPage(),
         // '/login': (_) => AuthScreen(),
-        '/home': (_) => MultiProvider(
-          providers: [
-            ChangeNotifierProvider(
-              create: (context) => MenuAppController(),
-            ),
-          ],
-          child: MainScreen(),
-        ),
+        '/home': (_) => MainScreen()
       },
     );
   }
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,13 +1,10 @@
-import 'package:admin/controllers/MenuAppController.dart';
 import 'package:admin/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: context.read<MenuAppController>().scaffoldKey,
       body: SafeArea(
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -19,6 +16,7 @@ class MainScreen extends StatelessWidget {
           ],
         ),
       ),
+      resizeToAvoidBottomInset: false,
     );
   }
 }
